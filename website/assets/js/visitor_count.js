edefca78d41a3d83af3524413098f277bb24f985
Kevin <printz.kevin@gmail.com>
2020-07-21 18:11:02 -0400

    add visitor count

---
+++ website/assets/js/visitor_count.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $.getJSON("https://us-east1-coffeetime-dev-282015.cloudfunctions.net/cloudresume-visitor-count", function(data){
+        console.log(data.currentVisitor);
+        $("#visitor-count").text("data.currentVisitor");
+    }).fail(function(){
+        console.log("An error has occurred while fetching the visitor count.");
+    });
+});

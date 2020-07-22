8a29d3ae2440b9f79baa91107565741ecb905cc4
Kevin <printz.kevin@gmail.com>
2020-07-21 22:14:19 -0400

    adding counter

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -115,10 +115,10 @@
   });
 
   // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+//  $('[data-toggle="counter-up"]').counterUp({
+//    delay: 10,
+//    time: 1000
+//  });
 
   // Skills section
   $('.skills-content').waypoint(function() {
